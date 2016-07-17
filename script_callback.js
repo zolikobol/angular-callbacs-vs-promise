edb639f6c9c14645c6d39ef6b0a334e7e3b91fe3
zolikobol <zoltan.kobol@globallogic.com>
2016-07-17 18:49:09 +0200

    added comment to new branch

--- script_callback.js
+++ script_callback.js
@@ -13,6 +13,8 @@ app.service('dataService', function($http) {
 
 //comments
 
+//ksjdalskdhaslkd
+
 app.controller('gitHubDataController', ['$scope','$http','dataService', function($scope, $http, dataService) {
   $scope.username = "globalthemes";
   dataService.getData($scope , function loadData($scope , result){
